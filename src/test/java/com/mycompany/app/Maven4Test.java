400f819e309e6dc655dce021fcf2da72dd363629
vishal <vishalpandit89@yahoo.co.in>
2019-05-02 18:23:27 +0530

    new file maven4test

---
+++ src/test/java/com/mycompany/app/Maven4Test.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class Maven4Test {
+	
+	@Test
+	public void Test1()
+	{
+		System.out.println("This is test1");
+	}
+	
+	@Test
+	public void gitTest()
+	{
+		System.out.println("This is git test");
+	}
+	
+	@Test
+	public void gitBranchTest()
+	{
+		System.out.println("This is git test");
+	}
+
+}
